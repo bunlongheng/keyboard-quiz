f5697db00b0cf447c87ac0c6d2ffd4159082f48b
Bunlong Heng <bheng.code@gmail.com>
2025-04-21 16:16:34 -0400

    chmod +x ./run-pi-server.sh && ./run-pi-server.sh

---
+++ run-pi-server.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd ~/keyboard-quiz/pi-server
+
+# Create venv if missing
+if [ ! -d "venv" ]; then
+  echo "📦 Creating virtualenv..."
+  python3 -m venv venv
+fi
+
+# Activate it
+source venv/bin/activate
+
+# Install packages if needed
+pip install -r requirements.txt
+
+# Run the display server
+echo "🚀 Running Display Server..."
+python app.py
